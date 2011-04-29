7de8c4da26c91aa06b1551b2c55861461f684b81
Romain Bochet <rom@bochet.fr>
2011-04-29 10:06:26 +0930

    Just works

---
+++ src/fr/stackr/android/ffreboot/Reboot.java
@@ -0,0 +1,24 @@
+package fr.stackr.android.ffreboot;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Reboot extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        try {
+			Runtime.getRuntime().exec(new String[]{"/system/bin/su","-c","reboot now"});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+    
+
+}
