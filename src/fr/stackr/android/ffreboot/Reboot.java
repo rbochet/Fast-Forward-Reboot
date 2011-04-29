a373d95c1cd4557f071f3026860ce35bf1ebff4f
Romain Bochet <rom@bochet.fr>
2011-04-29 11:01:42 +0930

    Button linked

--- src/fr/stackr/android/ffreboot/Reboot.java
+++ src/fr/stackr/android/ffreboot/Reboot.java
@@ -4,22 +4,30 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class Reboot extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        /*
-        try {
-			Runtime.getRuntime().exec(new String[]{"/system/bin/su","-c","reboot now"});
+public class Reboot extends Activity implements View.OnClickListener {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		((ImageButton) findViewById(R.id.restartButton))
+				.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		try {
+			Runtime.getRuntime().exec(
+					new String[] { "/system/bin/su", "-c", "reboot now" });
 		} catch (IOException e) {
 			e.printStackTrace();
-		} */
-
-    }
-    
+		}
+	}
 
 }
